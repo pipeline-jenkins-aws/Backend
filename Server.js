d517a02df5639d1f8538435fa34b67d4481c6a21
Dhwarakesh95 <82435431+Dhwarakesh95@users.noreply.github.com>
2024-11-09 09:56:50 -0500

    Update Server.js

--- Server.js
+++ Server.js
@@ -18,7 +18,7 @@
     app.use(express.json({limit : "10mb"})); //allows you to parse the body of the request
     app.use(cookieParser());
     app.use(cors({
-        origin: 'http://localhost:5173',  // your frontend URL
+        origin: 'http://whiskanddrizzle.dkdevops.online',  // your frontend URL
         credentials: true,                 // allow credentials (cookies, etc.)
       }));
       
